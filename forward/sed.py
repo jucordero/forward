81587787aec9e386c469f89818882e42da38447a
Juan <juan.cordero@postgrad.manchester.ac.uk>
2019-10-31 14:34:17 +0000

    Return SED for array of input redshifts

--- forward/sed.py
+++ forward/sed.py
@@ -1,16 +1,21 @@
 import numpy as np
 from astropy.io import fits
 
-def alphas(z, alpha_0, alpha_1, z1 = 1):
+def alphas(redshift, alpha_0, alpha_1, z1 = 1):
     """ For a given set of alpha_0, alpha_1 values, return the alpha values for the
     Dirichlet distribution at the given redshift z, evolving from z=0 to z=z1 (z1 = 1 by default)"""
 
-    return np.power(alpha_0, 1-(z/z1)) * np.power(alpha_1, (z/z1))
+    #shape = (redshift, alpha)
+    return np.array([[np.power(a0, 1-(z/z1)) * np.power(a1, (z/z1)) for a0,a1 in zip(alpha_0, alpha_1)] for z in redshift])
 
 def f_lambda(alphas, template_file, ext = 1):
     """ For a given set of alpha, return the c_i weighted SED """
-    ci = np.random.dirichlet(alphas)
+    nz, nt = alphas.shape
+    ci = np.zeros_like(alphas)
+    for i in range(nz):
+        ci[i] = np.random.dirichlet(alphas[i])
     #This assumes SED are on an image file with dimensions (n_lambda, n_template)
     templates = np.array(fits.open(template_file)[ext].data)
-    sed = ci*templates.sum(axis = 1)
+    sed = np.array([ci[i]*templates.T for i in range(nz)])
+    sed = sed.sum(axis=2)
     return sed
