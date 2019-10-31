ddc262036d6f72ad9c027a22a9db8ef29743dc02
Juan <juan.cordero@postgrad.manchester.ac.uk>
2019-10-31 12:12:41 +0000

    Fixed return statement

--- forward/sed.py
+++ forward/sed.py
@@ -5,7 +5,7 @@ def alphas(z, alpha_0, alpha_1, z1 = 1):
     """ For a given set of alpha_0, alpha_1 values, return the alpha values for the
     Dirichlet distribution at the given redshift z, evolving from z=0 to z=z1 (z1 = 1 by default)"""
 
-    return alpha = np.power(alpha_0, 1-(z/z1)) * np.power(alpha_1, (z/z1))
+    return np.power(alpha_0, 1-(z/z1)) * np.power(alpha_1, (z/z1))
 
 def f_lambda(alphas, template_file, ext = 1):
     """ For a given set of alpha, return the c_i weighted SED """
